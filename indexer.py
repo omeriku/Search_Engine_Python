d29d7630789556e4f019ae92578c3cef79e401d1
omern <nivom@post.bgu.ac.il>
2021-01-04 12:23:21 +0200

    indexer first insert to invertex and posting

--- indexer.py
+++ indexer.py
@@ -1,11 +1,18 @@
+from parser_module import Parse
+
 # DO NOT MODIFY CLASS NAME
 class Indexer:
     # DO NOT MODIFY THIS SIGNATURE
     # You can change the internal implementation as you see fit.
     def __init__(self, config):
-        self.inverted_idx = {{}}
+        self.parser = Parse()
+        self.inverted_idx = {}
         self.postingDict = {}
+        self.benchDataSet = {}
         self.config = config
+
+        # do for the entities parser rule
+        self.onceButTwiceLaterPosting = {}
 
     # DO NOT MODIFY THIS SIGNATURE
     # You can change the internal implementation as you see fit.
@@ -16,19 +23,80 @@ class Indexer:
         :param document: a document need to be indexed.
         :return: -
         """
-
+        # Get data that needs to be saved
         document_dictionary = document.term_doc_dictionary
+        self.benchDataSet[document.tweet_id] = document.term_doc_dictionary
+
+        # Check if the document dictionary isn't empty
+        if len(document_dictionary.keys()) != 0:
+            maxValueOfWordInDc = max(document_dictionary.values())
+            numberOfUniqueWords = len(document_dictionary.keys())
+            doc_len = document.doc_length
+        else:
+            return
+
         # Go over each term in the doc
         for term in document_dictionary.keys():
             try:
-                # Update inverted index and posting
-                if term not in self.inverted_idx.keys():
-                    self.inverted_idx[term] = 1
-                    self.postingDict[term] = []
-                else:
-                    self.inverted_idx[term] += 1
+                original_term = term
+                capitalTerm = term.upper()
+                lowerTerm = term.lower()
 
-                self.postingDict[term].append((document.tweet_id, document_dictionary[term]))
+                # save in seperate dictionary because maybe will be indexed later to disk - For entities
+                if term.__contains__(" ") and term not in self.parser.namesAndEntitiesMoreTimes:
+                    self.onceButTwiceLaterPosting[capitalTerm] = (
+                    document.tweet_id, document_dictionary[original_term], maxValueOfWordInDc, numberOfUniqueWords, doc_len)
+                    continue
+
+                # Check the upper lower case
+                if self.first_alfa_upper(term):
+                    term = capitalTerm
+                    # if exists in dict in capital
+                    if capitalTerm in self.inverted_idx.keys():
+                        self.inverted_idx[term] +=1
+                    # if exists in dict in lower then stay lower
+                    elif lowerTerm in self.inverted_idx.keys():
+                        term = lowerTerm
+                        self.inverted_idx[term] +=1
+                    else:
+                        # doesn't exist
+                        self.inverted_idx[term] = 1
+                        self.postingDict[term] = []
+                else:
+                    # Begins in small letter
+                    term = lowerTerm
+                    #if until now was capital
+                    if capitalTerm in self.inverted_idx.keys():
+                        # update in inverted_index dict to lower
+                        valueOfCapitalTerms = self.inverted_idx[capitalTerm]
+                        del self.inverted_idx[capitalTerm]
+                        self.inverted_idx[term] = valueOfCapitalTerms + 1
+                        # update in posting to lower
+                        if capitalTerm in self.postingDict.keys():
+                            value_of_posting = self.postingDict[capitalTerm]
+                            del self.postingDict[capitalTerm]
+                            self.postingDict[term] = value_of_posting
+                    # if saved in lower
+                    elif lowerTerm in self.inverted_idx.keys():
+                        self.inverted_idx[term] +=1
+                    else:
+                        # add - didn't exist
+                        self.inverted_idx[term] = 1
+                        self.postingDict[term] = []
+
+                # update posting dict
+                self.postingDict[term].append(
+                    (document.tweet_id, document_dictionary[original_term], maxValueOfWordInDc, numberOfUniqueWords,
+                     doc_len))
+
+                # # Update inverted index and posting
+                # if term not in self.inverted_idx.keys():
+                #     self.inverted_idx[term] = 1
+                #     self.postingDict[term] = []
+                # else:
+                #     self.inverted_idx[term] += 1
+                #
+                # self.postingDict[term].append((document.tweet_id, document_dictionary[term]))
 
             except:
                 print('problem with the following key {}'.format(term[0]))
@@ -67,4 +135,14 @@ class Indexer:
         """
         Return the posting list from the index for a term.
         """
-        return self.postingDict[term] if self._is_term_exist(term) else []
+        return self.postingDict[term] if self._is_term_exist(term) else None
+
+
+    # find if the letter is upper lower
+    def first_alfa_upper(self, word):
+        for c in word:
+            if c.isalpha():
+                if c.isupper():
+                    return True
+                return False
+        return False
